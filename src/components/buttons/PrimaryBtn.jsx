3a9e802b9a4f4afa4a247fa475e2f994c4c73471
rajitha-00 <rajithatw07102@gmail.com>
2023-06-14 01:58:06 +0530

    Dashboard done

--- src/components/buttons/PrimaryBtn.jsx
+++ src/components/buttons/PrimaryBtn.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Button } from 'reactstrap'
 
 const PrimaryBtn = (props) => {
   return (
     <>
-        <Button color="success">
+        <Button color="success" className='px-7 w-full my-2'>
             {props.btnName}
         </Button>
     </>
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
-
+import {  Card,  Col, Row } from 'reactstrap'
+import PrimaryBtn from '../components/buttons/PrimaryBtn'
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
-    </div>
+    <>
+      <Row>
+        <Col >
+          <Card body>
+            <Row>
+              <Col sm="6" md="4">
+                <PrimaryBtn
+                  btnName="Students"
+                />
+              </Col>
+              <Col sm="6" md="4">
+                <PrimaryBtn
+                  btnName="Teachers"
+                />
+              </Col>
+              <Col sm="6" md="4">
+                <PrimaryBtn
+                  btnName="Subjects"
+                />
+              </Col>
+              <Col sm="6" md="4">
+                <PrimaryBtn
+                  btnName="Classrooms"
+                />
+              </Col>
+              <Col sm="6" md="4">
+                <PrimaryBtn
+                  btnName="Assign Subjects"
+                />
+              </Col>
+              <Col sm="6" md="4">
+                <PrimaryBtn
+                  btnName="Assign Classes"
+                />
+              </Col>
+              <Col sm="6" md="4">
+                <PrimaryBtn
+                  btnName="Students Reports"
+                />
+              </Col>
+            </Row>
+             
+          </Card>
+        </Col>
+       
+      </Row>
+    </>
   )
 }
 
