c43d9132c4a4023f12f57e0fa25862517f36d525
rajitha-00 <rajithatw07102@gmail.com>
2023-06-14 01:44:22 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-
+import { Route, Routes } from "react-router-dom";
+import NavbarLms from "./components/NavbarLms";
+import Dashboard from "./views/Dashboard"
+import StudentsReport from "./views/StudentsReport";
+import Students from "./views/Students";
+import Subjects from "./views/Subjects";
+import Teachers from "./views/Teachers";
+import ClassRooms from "./views/ClassRooms";
+import AssignClass from "./views/AssignClass";
+import AssignSubject from "./views/AssignSubject";
 
 
 function App() {
   return (
     <>
+      <NavbarLms />
+      <Routes>
+        <Route path='/' element= {<Dashboard/>}/>
+        <Route path='/students' element= {<Students/>}/>
+        <Route path='/stdentreport' element= {<StudentsReport/>}/>
+        <Route path='/subjects' element= {<Subjects/>}/>
+        <Route path='/teachers' element= {<Teachers/>}/>
+        <Route path='/classes' element= {<ClassRooms/>}/>
+        <Route path='/assignclass' element= {<AssignClass/>}/>
+        <Route path='/assignsubject' element= {<AssignSubject/>}/>
+      </Routes>
     </>
   );
 }
---
+++ src/components/NavbarLms.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+} from 'reactstrap';
+
+const NavbarLms = () => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  const closeNavbar = () => setCollapsed(true); // Function to close the navbar
+
+  return (
+    <>
+      <Navbar color="faded" light>
+        <NavbarToggler onClick={toggleNavbar} className="me-auto" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <Link to="/" onClick={closeNavbar}>Dashboard</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/students" onClick={closeNavbar}>Students</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/teachers" onClick={closeNavbar}>Teachers</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/classes" onClick={closeNavbar}>Classrooms</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/subjects" onClick={closeNavbar}>Subjects</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/stdentreport" onClick={closeNavbar}>Students Reports</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/assignsubject" onClick={closeNavbar}>Assign Subjects</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/assignclass" onClick={closeNavbar}>Assign Classes</Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+        <Link href="/" className="me-2">
+          reactstrap
+        </Link>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavbarLms;
