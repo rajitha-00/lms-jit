6786eb85402443a833fbcc96bfc58e7fe1800fd7
rajitha-00 <rajithatw07102@gmail.com>
2023-06-14 02:37:55 +0530

    Subjects and classes 50%  doen

--- src/views/ClassRooms.jsx
+++ src/views/ClassRooms.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem,
+} from 'reactstrap';
+import DataTable from '../components/DataTable';
 
 const ClassRooms = () => {
+  const [openAccordion1, setOpenAccordion1] = useState('1');
+  const [openAccordion2, setOpenAccordion2] = useState('1');
+
+  const toggleAccordion1 = (id) => {
+    if (openAccordion1 === id) {
+      setOpenAccordion1('');
+    } else {
+      setOpenAccordion1(id);
+    }
+  };
+
+  const toggleAccordion2 = (id) => {
+    if (openAccordion2 === id) {
+      setOpenAccordion2('');
+    } else {
+      setOpenAccordion2(id);
+    }
+  };
+
+  const headers = ['Subject ID', 'Classroom'];
+  const data = [
+    ['1', 'Class A'],
+    ['2', 'Class B'],
+    ['3', 'Class C'],
+    ['4', 'Class D'],
+    ['5', 'Class E']
+  ];
+  
   return (
     <>
-      
+      <Accordion open={openAccordion1} toggle={toggleAccordion1}>
+        <AccordionItem>
+          <AccordionHeader targetId="1">Class Details</AccordionHeader>
+          <AccordionBody accordionId="1">
+            
+          </AccordionBody>
+        </AccordionItem>
+       
+      </Accordion>
+
+      <Accordion className='mt-5' open={openAccordion2} toggle={toggleAccordion2}>
+        <AccordionItem>
+          <AccordionHeader targetId="1">Exsisting Classes</AccordionHeader>
+          <AccordionBody accordionId="1">
+            <DataTable
+            headers={headers} data={data}
+            />
+          </AccordionBody>
+        </AccordionItem>
+       
+      </Accordion>
     </>
   )
 }
--- src/views/Subjects.jsx
+++ src/views/Subjects.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem,
+} from 'reactstrap';
+import DataTable from '../components/DataTable';
 
 const Subjects = () => {
+  const [openAccordion1, setOpenAccordion1] = useState('1');
+  const [openAccordion2, setOpenAccordion2] = useState('1');
+
+  const toggleAccordion1 = (id) => {
+    if (openAccordion1 === id) {
+      setOpenAccordion1('');
+    } else {
+      setOpenAccordion1(id);
+    }
+  };
+
+  const toggleAccordion2 = (id) => {
+    if (openAccordion2 === id) {
+      setOpenAccordion2('');
+    } else {
+      setOpenAccordion2(id);
+    }
+  };
+
+  const headers = ['Subject ID', 'Subject Name'];
+  const data = [
+    ['001', 'Mathematics'],
+    ['002', 'Science'],
+    ['003', 'English'],
+    ['004', 'History'],
+    ['005', 'Geography']
+  ];
+  
   return (
     <>
-      
+      <Accordion open={openAccordion1} toggle={toggleAccordion1}>
+        <AccordionItem>
+          <AccordionHeader targetId="1">Subject Details</AccordionHeader>
+          <AccordionBody accordionId="1">
+            
+          </AccordionBody>
+        </AccordionItem>
+       
+      </Accordion>
+
+      <Accordion className='mt-5' open={openAccordion2} toggle={toggleAccordion2}>
+        <AccordionItem>
+          <AccordionHeader targetId="1">Exsisting Subjects</AccordionHeader>
+          <AccordionBody accordionId="1">
+            <DataTable
+            headers={headers} data={data}
+            />
+          </AccordionBody>
+        </AccordionItem>
+       
+      </Accordion>
     </>
   )
 }
